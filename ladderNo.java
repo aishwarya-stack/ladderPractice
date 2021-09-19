a529c42460c5ebcc1da411ce3ae838a2d9a746fe
aishwarya-stack <aishwaryaashokkadam@gmail.com>
2021-09-19 22:42:51 +0530

    ADD:players roll die between 1 to 6

--- ladderNo.java
+++ ladderNo.java
@@ -1,11 +1,51 @@
 package ladderNo;
 
 public class ladderNo {
-	static int max=0;
+	static int position=0;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome ");
 		
-	}
+		{
+			//System.out.println("Welcome ");
+			//System.out.println("Welcome new player");
+			
 
+			int choice=(int)(Math.random()*3)+1;
+			//.out.print("enter your choice is"+choice);
+			switch (choice) {
+			
+		    case 1:   System.out.println("here is Ladder");
+		              
+		              int number=(int)(Math.random()*6)+1;
+			          System.out.println("number you received is"+number);
+			          position=position+number;
+			          
+		             break;
+		    case 2:  System.out.println("here is snake");
+		              number=(int)(Math.random()*6)+1;
+			         System.out.println("number you received is"+number);
+			         if(position==0)
+			         {
+			        	 position=0; 
+			         }
+			         else
+			         {
+			        	 position=position-number;
+			         }
+			         if(position<0)
+			         {
+			        	 position=1; 
+			         }
+		             break;
+		    
+		    default:System.out.println("no switch");
+		             break;
+		             
+		             
+		             
+		             }
+		
+		     }
+}
 }
