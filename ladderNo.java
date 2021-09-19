9fb72c33c9859004aef9281c520263c022a613a5
aishwarya-stack <aishwaryaashokkadam@gmail.com>
2021-09-19 22:33:19 +0530

    ADD:start wit zero

--- ladderNo.java
+++ ladderNo.java
@@ -1,10 +1,11 @@
 package ladderNo;
 
 public class ladderNo {
-
+	static int max=0;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome ");
+		
 	}
 
 }
